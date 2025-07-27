aa4241fd0dbdc30fc5556d1628c322866bcc6831
Rich Gregorio <thekrunchyfrog@gmail.com>
2025-07-27 16:21:18 -0400

    keep trying until you get a value

--- thermometer.py
+++ thermometer.py
@@ -9,9 +9,17 @@ class Thermometer:
         self.dht_device = adafruit_dht.DHT22(board.D4)
     
     def readTemp(self):
-        temperature_c = self.dht_device.temperature
+        temperature_c = None
+        humidity = None
+        
+        while temperature_c is None:
+            temperature_c = self.dht_device.temperature
+        
         temperature_f = temperature_c * (9 / 5) + 32
-        humidity = self.dht_device.humidity
+        
+        while humidity is None:
+            humidity = self.dht_device.humidity
+        
         return {"temp": temperature_f, "humidity": humidity}
 
     def close(self):
