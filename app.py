6775693abe505d6f7f792b9f6d050fbff2e5c8d5
Richard D. Gregorio, Jr <rich@krunchylabs.com>
2025-09-12 14:02:30 -0400

    read from db

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import adafruit_dht
-import board
-import time
+from temperaturedb import TemperatureDB
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,37 +6,10 @@ app = Flask(__name__)
 
 @app.route("/garage", methods=["GET"])
 def get_garage_data():
-    tempList = []
+    conn = TemperatureDB("temphistory.db")
+    garage_data = conn.readLastTemp()
+    return garage_data
 
-    for x in range(0, 3):
-        try:
-            weather = readTemp()
-        except RuntimeError as err:
-            continue
-        tempList.append(weather)
-        time.sleep(2)
-
-    garage_data = tempList[1]
-    return jsonify(garage_data)
-
-def readTemp():
-    dht_device = adafruit_dht.DHT22(board.D4)
-    
-    temperature_c = None
-    humidity = None
-        
-    while temperature_c is None:
-        temperature_c = dht_device.temperature
-        
-    temperature_f = temperature_c * (9 / 5) + 32
-        
-    while humidity is None:
-         humidity = dht_device.humidity
-        
-    dht_device.exit()
-        
-    return {"temperature": temperature_f, "humidity": humidity}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
-
